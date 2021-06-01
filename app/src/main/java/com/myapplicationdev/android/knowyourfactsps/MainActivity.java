ff127ac45e5403e5d66f206b312186ac5b6fff35
JonathanZYZ <jonathanzengyuzhao@gmail.com>
2021-06-01 11:57:30 +0800

    ViewPager

--- app/src/main/java/com/myapplicationdev/android/knowyourfactsps/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/knowyourfactsps/MainActivity.java
@@ -2,17 +2,51 @@ package com.myapplicationdev.android.knowyourfactsps;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+
+    Button btnLater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewpager1);
+
+        btnLater = findViewById(R.id.btnLater);
+
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        btnLater.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 
     @Override
